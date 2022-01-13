9cb496c4bbfbaf4d8f06e5ed6b6fdece1653a908
hamed <40214048+hamedsahebi@users.noreply.github.com>
2022-01-13 18:46:19 +0330

    The mongoose object fixed

--- startup/db.js
+++ startup/db.js
@@ -2,8 +2,25 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
+// module.exports = function() {
+//   const db = config.get('db');
+
+
+//   mongoose.connect(db)
+//     .then(() => winston.info(`Connected to ${db}...`));
+// }
+
+
 module.exports = function() {
   const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
+
+
+  try{
+  mongoose.connect(db,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    ()=>console.log("Mongoose is connected"))
+  } catch(e) {
+    console.log("could not connect");
+  }
+   
 }
